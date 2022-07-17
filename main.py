49e95b90d16c6cc25e8595e79c9620f05b2d84b9
4ll0w3v1l <betahellatester@gmail.com>
2022-07-17 21:28:18 +0300

    server-side numbering, no local storage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import json
 
 app = FastAPI()
 
@@ -9,21 +8,14 @@ templates = Jinja2Templates(directory='templates')
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    with open('data.json', mode='r', encoding='utf-8') as f:
-        json_data = json.load(f)
-    return templates.TemplateResponse("messages.html", {"request": request, 'savedMessages': json_data['messages']})
+    return templates.TemplateResponse("messages.html", {"request": request})
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    i = 0
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-
-        with open('data.json', mode='r+', encoding='utf-8') as f:
-            json_data = json.load(f)
-            json_data['messages'].append(data)
-            f.seek(0)
-            json.dump(json_data, f, indent=4)
-
-        await websocket.send_text(f"Message text: {data}")
+        i += 1
+        await websocket.send_text(f"{i}: {data}")
